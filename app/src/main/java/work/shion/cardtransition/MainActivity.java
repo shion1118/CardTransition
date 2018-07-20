c839b126e80460532c97da685c10784ac275cda5
Shion Fukushima <shfukush@yahoo-corp.jp>
2018-07-20 21:12:22 +0900

    Add shared element

--- app/src/main/java/work/shion/cardtransition/MainActivity.java
+++ app/src/main/java/work/shion/cardtransition/MainActivity.java
@@ -1,13 +1,32 @@
 package work.shion.cardtransition;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.ActivityOptions;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.card.MaterialCardView;
+import android.support.v4.app.ActivityOptionsCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    private MaterialCardView userCard;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        userCard = (MaterialCardView) findViewById(R.id.user_cardView);
+        userCard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ActivityOptions options = ActivityOptions.
+                        makeSceneTransitionAnimation(MainActivity.this, (View) userCard, "user");
+
+                Intent intent = new Intent(MainActivity.this, UserActivity.class);
+                startActivity(intent, options.toBundle());
+            }
+        });
     }
 }
