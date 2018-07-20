ed994aef8f32cac08bac76cd63ca612e3b9407b5
Shion Fukushima <shfukush@yahoo-corp.jp>
2018-07-20 22:10:19 +0900

    Add edit text to user profile

--- app/src/main/java/work/shion/cardtransition/UserActivity.java
+++ app/src/main/java/work/shion/cardtransition/UserActivity.java
@@ -2,12 +2,22 @@ package work.shion.cardtransition;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class UserActivity extends AppCompatActivity {
+
+    private EditText editName;
+    private EditText editPhone;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user);
+
+        editName = (EditText) findViewById(R.id.user_name_editText);
+        editName.setText(getString(R.string.user_name));
+
+        editPhone = (EditText) findViewById(R.id.user_phone_editText);
+        editPhone.setText(getString(R.string.user_phone));
     }
 }
